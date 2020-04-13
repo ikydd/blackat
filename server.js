73925baa86005264f6c0258a4c468378c9f94bc5
Iain Kydd <iain.kydd@gmail.com>
2020-04-13 22:07:09 +0100

    Add linting

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-const { createReadStream } = require('fs');
-const Koa = require('koa'); 
-const KoaRouter = require('koa-router');
-const serve = require('koa-static');
-const path = require('path');
+const { createReadStream } = require("fs");
+const Koa = require("koa");
+const KoaRouter = require("koa-router");
+const serve = require("koa-static");
+const path = require("path");
+
 const port = process.env.PORT || 5000;
 
-const app = new Koa(); 
+const app = new Koa();
 const router = new KoaRouter();
 
-router.get('/api/cards', (ctx) => {
-    ctx.set('Content-Type', 'application/json');
-    ctx.body = createReadStream(path.join(__dirname, 'api/cards.json'));
+router.get("/api/cards", (ctx) => {
+  ctx.set("Content-Type", "application/json");
+  ctx.body = createReadStream(path.join(__dirname, "api/cards.json"));
 });
 
-app
-    .use(router.routes())
-    .use(router.allowedMethods())
+app.use(router.routes()).use(router.allowedMethods());
 
-app.use(serve(path.join(__dirname, 'client/build'), {
-    gzip: true
-}));
+app.use(
+  serve(path.join(__dirname, "client/build"), {
+    gzip: true,
+  })
+);
 
 const start = () => app.listen(port);
 
 if (!module.parent) {
-    start();
+  start();
 }
 
 module.exports = {
-    start
-}
+  start,
+};
---
+++ server.test.js
@@ -0,0 +1,5 @@
+describe("server", () => {
+  it("tests", () => {
+    expect(true).toBe(false);
+  });
+});
