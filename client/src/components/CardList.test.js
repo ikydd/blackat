f7fe5ce0d97f5d9dfac098bb2f33b47afca30267
Iain Kydd <iain.kydd@gmail.com>
2020-04-17 21:34:26 +0100

    Finally sort out proper testing of async function

--- client/src/components/CardList.test.js
+++ client/src/components/CardList.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { create, act  }  from 'react-test-renderer';
-import waitUntil from 'async-wait-until';
-import { shallow, mount } from 'enzyme'
+import { create }  from 'react-test-renderer';
+import { waitFor } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import CardList from './CardList';
 import Card from './Card';
 import * as api from '../helpers/api';
 
 jest.mock('../helpers/api');
+jest.mock('./Card', () => 'Card');
 
 describe('CardList', () => {
-
   let mockData = [
     {
       name: 'foo1',
@@ -33,27 +33,23 @@ describe('CardList', () => {
     shallow(<CardList/>);
   });
 
+  it('renders with no cards to begin with', async () => {
+    const component = create(<CardList/>);
+
+    expect(component.root.findAllByType(Card).length).toEqual(0);
+  })
+
   it('uses api.call correctly', () => {
     shallow(<CardList/>);
 
     expect(api.call).toHaveBeenCalledWith('/cards');
   });
 
-  it('renders with no cards to begin with', async () => {
-    let component;
-    act(() => {
-      component = create(<CardList/>);
-    });
+  it('renders with the right number of cards', async () => {
+    const component = create(<CardList/>);
 
-    expect(component.findAllByType(Card).length).toEqual(0);
-  })
+    await waitFor(() => component.root.findAllByType('Card').length > 0)
 
-  it('renders with multiple cards', async () => {
-    let component;
-    act(() => {
-      component = create(<CardList/>);
-    });
-
-    expect(component.findAllByType(Card).length).toEqual(3);
+    expect(component.root.findAllByType('Card').length).toEqual(3);
   })
 });
