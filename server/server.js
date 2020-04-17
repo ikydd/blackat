e0332152d905901647befc566475d97685b5a575
Iain Kydd <iain.kydd@gmail.com>
2020-04-17 18:46:22 +0100

    Add server files

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const { createReadStream } = require("fs");
+const Koa = require("koa");
+const KoaRouter = require("koa-router");
+const serve = require("koa-static");
+const path = require("path");
+
+const port = process.env.PORT || 5000;
+
+const app = new Koa();
+const router = new KoaRouter();
+
+router.get("/status", (ctx) => {
+  ctx.status = 200;
+  ctx.body = "OK";
+});
+
+router.get("/api/cards", (ctx) => {
+  ctx.set("Content-Type", "application/json");
+  ctx.body = createReadStream(path.join(__dirname, "data", "cards.json"));
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.use(
+  serve(path.join(__dirname, "..", "client", "build"), {
+    gzip: true,
+  })
+);
+
+if (!module.parent) {
+  app.listen(port);
+}
+
+module.exports = {
+  app,
+};
---
+++ server/server.test.js
@@ -0,0 +1,49 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const mock = require("mock-fs");
+const { agent } = require("supertest");
+const { app } = require("./server");
+
+describe("server", () => {
+  let request;
+  let server;
+
+  beforeEach(() => {
+    server = http.createServer(app.callback());
+    request = agent(server);
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+
+  describe("status", () => {
+    it("return OK", () => {
+      return request.get("/status").expect(200, "OK");
+    });
+  });
+
+  describe("api", () => {
+    const cards = fs.readFileSync(
+      path.join(__dirname, "fixtures", "cards.json")
+    );
+
+    beforeEach(() => {
+      mock({
+        data: {
+          "cards.json": cards,
+        },
+      });
+    });
+
+    afterEach(mock.restore);
+
+    it("return cards data", () => {
+      return request
+        .get("/api/cards")
+        .expect("Content-Type", /json/)
+        .expect(200, JSON.parse(cards));
+    });
+  });
+});
