10864b9c220025c16254a01074f3918e04f91ac0
Iain Kydd <iain.kydd@gmail.com>
2020-04-18 18:12:11 +0100

    Add default import behaviour

--- import/src/importer.js
+++ import/src/importer.js
@@ -1,10 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
 class Importer {
-    constructor(api) {
+    constructor(api, importDir) {
+        if (!api) {
+            throw new Error('An API is required in the Importer');
+        }
+        if (!importDir) {
+            throw new Error('An import directory is required in the Importer');
+        }
+
         this.api = api;
+        this.importDirectory = importDir;
     }
 
-    cards() {
-        return this.api.call('/cards');
+    import(endpoint) {
+        return this.api.call(endpoint)
+            .then((data) => {
+                const importPath = path.join(this.importDirectory, 'cards.json');
+                return fs.writeFile(importPath, JSON.stringify(data));
+            })
     }
 }
 
--- import/src/importer.test.js
+++ import/src/importer.test.js
@@ -1,17 +1,44 @@
 const SourceApi = require('./source-api');
 const Importer = require('./importer');
+const fs = require('fs');
+
+jest.mock('fs');
 
 describe('Importer', () => {
-    describe('cards', () => {
-        it('calls the cards endpoint', () => {
-            const mockData = {};
-            const foo = new SourceApi();
-            foo.call = jest.fn(() => mockData);
-            const importer = new Importer(foo);
-            importer.cards();
+    describe('import', () => {
 
-            expect(foo.call).toHaveBeenCalledWith('/cards');
+        it('errors if you do not pass in an API', () => {
+            expect(() => { new Importer(null, './test/'); }).toThrow('An API is required in the Importer');
         });
 
+        it('errors if you do not pass in a import directory', () => {
+            const foo = new SourceApi();
+            expect(() => { new Importer(foo); }).toThrow('An import directory is required in the Importer');
+        });
+
+        it('calls the requested endpoint', async () => {
+            const mockData = {};
+
+            const foo = new SourceApi();
+            foo.call = jest.fn(() => Promise.resolve(mockData));
+
+            const importer = new Importer(foo, './test/');
+            await importer.import('/bar');
+
+            expect(foo.call).toHaveBeenCalledWith('/bar');
+        });
+
+        it('writes the data to a file', async () => {
+            const mockData = { foo: 'bar' };
+            const importFolder = 'test/';
+
+            const foo = new SourceApi();
+            foo.call = jest.fn(() => Promise.resolve(mockData));
+
+            const importer = new Importer(foo, importFolder);
+            await importer.import('/bar');
+
+            expect(fs.writeFile).toHaveBeenCalledWith(`${importFolder}cards.json`, JSON.stringify(mockData));
+        });
     });
 })
