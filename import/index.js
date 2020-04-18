797091c2de364f9e2d8039162cb77f75ce8699eb
Iain Kydd <iain.kydd@gmail.com>
2020-04-19 00:41:32 +0100

    Point importer at real data store

--- import/index.js
+++ import/index.js
@@ -3,6 +3,8 @@ const SourceApi = require('./src/source-api');
 const Importer = require('./src/importer');
 
 const nrdb = new SourceApi('https://netrunnerdb.com/api/2.0/public');
-const importer = new Importer(nrdb, path.join(__dirname, 'data-tmp'));
+const importer = new Importer(nrdb, path.join(__dirname, '..', 'data'));
 
-importer.import('/cards');
+importer.import('/cards', (data) => {
+    return data.data;
+});
