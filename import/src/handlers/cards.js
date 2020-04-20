d3ffccd75abaa43a10ac3278948a1c52271c5d3e
Iain Kydd <iain.kydd@gmail.com>
2020-04-20 18:44:52 +0100

    Add title and image replacement

--- import/src/handlers/cards.js
+++ import/src/handlers/cards.js
@@ -1,6 +1,10 @@
-const handler = (data) => {
-    return data.data;
-  }
+const handler = ({ imageUrlTemplate, data: cards }) => cards.map(({ title, code, imagesrc }) => {
+        const img = imagesrc || imageUrlTemplate.replace('{code}', code);
+        return {
+            title,
+            imagesrc: img
+        }
+    });
 
 module.exports = {
     handler
--- import/src/handlers/cards.test.js
+++ import/src/handlers/cards.test.js
@@ -4,15 +4,41 @@ const mockData = {
     "imageUrlTemplate": "https://netrunnerdb.com/card_image/{code}.png",
     "data": [
       {
-        name: "Account Siphon"
+        title: "Medium",
+        code: "01010"
+      },
+      {
+        title: "Gordian Blade",
+        code: "01043"
+      },
+      {
+        title: "HQ Interface",
+        code: "01010",
+        imagesrc: "http://www.cardgamedb.com/forums/uploads/an/med_ADN49_26.png"
       }
     ]
 };
 
 describe('cards handler', () => {
-    it('returns the data property', () => {
+
+    it('returns the cards', () => {
         const output = handler(mockData);
 
-        expect(output).toEqual(mockData.data);
+        expect(output[0].title).toEqual(mockData.data[0].title);
+        expect(output[1].title).toEqual(mockData.data[1].title);
+        expect(output[2].title).toEqual(mockData.data[2].title);
+    });
+
+    it('adds an image URL to each card', () => {
+        const output = handler(mockData);
+
+        expect(output[0].imagesrc).toEqual(`https://netrunnerdb.com/card_image/${mockData.data[0].code}.png`);
+        expect(output[1].imagesrc).toEqual(`https://netrunnerdb.com/card_image/${mockData.data[1].code}.png`);
+    });
+
+    it('adds leaves a custom image if already present', () => {
+        const output = handler(mockData);
+
+        expect(output[2].imagesrc).toEqual(mockData.data[2].imagesrc);
     });
 })
