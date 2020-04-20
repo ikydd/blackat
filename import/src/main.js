1a8c8c88cddd9f6278f257af39e2821ccb174785
Iain Kydd <iain.kydd@gmail.com>
2020-04-20 18:21:44 +0100

    Tidy up some constants

--- import/src/main.js
+++ import/src/main.js
@@ -3,9 +3,12 @@ const SourceApi = require("./source-api");
 const Importer = require("./importer");
 const cardsHandler = require("./handlers/cards");
 
+const nrdbApi = "https://netrunnerdb.com/api/2.0/public";
+const importDir = path.join(__dirname, "..", "..", "data");
+
 const start = async () => {
-  const nrdb = new SourceApi("https://netrunnerdb.com/api/2.0/public");
-  const importer = new Importer(nrdb, path.join(__dirname, "..", "..", "data"));
+  const nrdb = new SourceApi(nrdbApi);
+  const importer = new Importer(nrdb, importDir);
 
   importer.import("/cards", cardsHandler);
 };
