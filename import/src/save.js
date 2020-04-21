812f1fd79c25d0eb6d801ffee49997887ccdb3fa
Iain Kydd <iain.kydd@gmail.com>
2020-04-21 13:02:20 +0100

    Rename and simplify importer

---
+++ import/src/save.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const save = async (data, filepath) => {
+  if (!data) {
+    throw new Error("Some data is required to save");
+  }
+  if (!filepath) {
+    throw new Error("A file path is required to save data");
+  }
+  return new Promise((resolve) =>
+    fs.writeFile(filepath, JSON.stringify(data), resolve)
+  );
+}
+
+module.exports = save;
---
+++ import/src/save.test.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const save = require("./save");
+
+jest.mock("fs");
+
+describe("save", () => {
+  beforeEach(() => {
+    fs.writeFile.mockImplementation((path, data, callback) => callback());
+  });
+
+  afterEach(() => {
+    fs.writeFile.mockReset();
+  });
+
+  it("errors if you do not pass in data", () => {
+    expect(save(null, "./test/bar.json")).rejects.toThrow(
+      "Some data is required to save"
+    );
+  });
+
+  it("errors if you do not pass in a file path", () => {
+    const mockData = {}
+
+    expect(save(mockData)).rejects.toThrow(
+      "A file path is required to save data"
+    );
+  });
+
+  it("calls the requested endpoint", async () => {
+    const mockData = { foo: "bar" };
+
+    await save(mockData, "./test/bar.json");
+
+    expect(fs.writeFile).toHaveBeenCalledWith(
+      "./test/bar.json",
+      JSON.stringify(mockData),
+      expect.any(Function)
+    );
+  });
+});
