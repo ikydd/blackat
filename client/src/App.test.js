25ad1acec266c78fe931dc61289f0f53bdfe5513
Iain Kydd <iain.kydd@gmail.com>
2020-05-01 23:03:54 +0100

    Add basic component presence tests to App

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { create }  from 'react-test-renderer';
 import App from './App';
+import ControlPanel from './components/ControlPanel';
+import CardList from './components/CardList';
+
+jest.mock('./components/ControlPanel');
+jest.mock('./components/CardList');
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('contains the ControlPanel', () => {
+  const component = create(<App />);
+  expect(component.root.findAllByType(ControlPanel).length).toEqual(1);
+});
+
+it('contains the CardList', () => {
+  const component = create(<App />);
+  expect(component.root.findAllByType(CardList).length).toEqual(1);
 });
