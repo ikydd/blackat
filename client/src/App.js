d2783e2edc6608c748d482b796cd01b7d4f3b943
Iain Kydd <iain.kydd@gmail.com>
2020-05-01 22:57:58 +0100

    Remove Header for App

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CardList from './components/CardList';
-import Header from './components/Header';
 import ControlPanel from './components/ControlPanel';
 import './App.css';
 
