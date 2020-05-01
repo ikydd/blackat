99a333fb254860f2ebe007c05481d9a58ad6d390
Iain Kydd <iain.kydd@gmail.com>
2020-05-01 23:04:32 +0100

    Remove redundant aysnc keywords

--- client/src/components/ControlPanel.test.js
+++ client/src/components/ControlPanel.test.js
@@ -13,22 +13,22 @@ describe('CardList', () => {
     shallow(<ControlPanel/>);
   });
 
-  it('has a header', async () => {
+  it('has a header', () => {
     const component = create(<ControlPanel/>);
     expect(component.root.findAllByType(Header).length).toEqual(1);
   });
 
-  it('has two sides', async () => {
+  it('has two sides', () => {
     const component = create(<ControlPanel/>);
     expect(component.root.findAllByType(SideButton).length).toEqual(2);
   });
 
-  it('has Runner as the first side', async () => {
+  it('has Runner as the first side', () => {
     const component = create(<ControlPanel/>);
     expect(component.root.findAllByType(SideButton)[0].props.title).toEqual('Runner');
   });
 
-  it('has Corp as the second side', async () => {
+  it('has Corp as the second side', () => {
     const component = create(<ControlPanel/>);
     expect(component.root.findAllByType(SideButton)[1].props.title).toEqual('Corp');
   });
