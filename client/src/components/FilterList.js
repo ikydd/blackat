7c199d4f81a983e66e08f1604164609144195eb1
Iain Kydd <iain.kydd@gmail.com>
2020-06-12 22:17:35 +0100

    Markup adjustments to checkboxes

--- client/src/components/FilterList.js
+++ client/src/components/FilterList.js
@@ -3,7 +3,7 @@ import { call } from '../helpers/api';
 import Icon from './Icon.js';
 import './FilterList.css';
 
-class Checklist extends Component {
+class FilterList extends Component {
 
   state = {
       options: [],
@@ -44,10 +44,10 @@ class Checklist extends Component {
       <div id="factions" className="filter-list">
         <h4 className="filter-list-title">Factions</h4>
         {this.state.options.filter(this.filterBySide).map((faction) => (
-        <div key={faction.code} >
+        <div key={faction.code} className="checkbox">
           <label htmlFor={'faction-filter-' + faction.code}>
             <input type="checkbox" id={'faction-filter-' + faction.code} name={faction.code} value={faction.code} checked={this.isSelected(faction)} onChange={this.change(faction)} />
-            &nbsp; <Icon code={faction.code} /> {faction.name}
+            &nbsp; <Icon code={faction.code}/> {faction.name}
           </label>
         </div>
       ))}</div>
@@ -55,4 +55,4 @@ class Checklist extends Component {
   }
 }
 
-export default Checklist;
+export default FilterList;
