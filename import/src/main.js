2f1ddc1c8f231f8067bc1c1c03de73620ad8091e
Iain Kydd <iain.kydd@gmail.com>
2020-07-24 18:02:21 +0100

    Add pack import to main import script

--- import/src/main.js
+++ import/src/main.js
@@ -1,11 +1,13 @@
 const cards = require("./cards/import");
 const factions = require("./factions/import");
 const types = require("./types/import");
+const packs = require("./packs/import");
 
 const run = async () => {
   await cards();
   await factions();
   await types();
+  await packs();
 };
 
 module.exports = {
--- import/src/main.test.js
+++ import/src/main.test.js
@@ -1,9 +1,13 @@
 const cards = require("./cards/import");
 const factions = require("./factions/import");
+const types = require("./types/import");
+const packs = require("./packs/import");
 const main = require("./main");
 
 jest.mock("./cards/import");
 jest.mock("./factions/import");
+jest.mock("./types/import");
+jest.mock("./packs/import");
 
 describe("main", () => {
   it("calls cards", async () => {
@@ -11,14 +15,22 @@ describe("main", () => {
 
     expect(cards).toHaveBeenCalled();
   });
+
   it("calls factions", async () => {
     await main.run();
 
     expect(factions).toHaveBeenCalled();
   });
+
   it("calls types", async () => {
     await main.run();
 
-    expect(factions).toHaveBeenCalled();
+    expect(types).toHaveBeenCalled();
+  });
+
+  it("calls packs", async () => {
+    await main.run();
+
+    expect(packs).toHaveBeenCalled();
   });
 });
