fa267acf8dc641ba96c59555a318294b0dcc489e
Iain Kydd <iain.kydd@gmail.com>
2020-08-01 17:32:25 +0100

    Refactor image download to use path

--- import/src/cards/download-images.js
+++ import/src/cards/download-images.js
@@ -1,7 +1,8 @@
 const fs = require('fs-extra');
 const axios = require('axios');
+const path = require('path');
 
-const cardPath = (card, path) => `${path}/${card.code}.png`;
+const cardPath = (card, target) => path.join(target, `${card.code}.png`);
 const existingCards = (path) => (card) => {
     const file = cardPath(card, path);
     return !fs.existsSync(file) || fs.statSync(file)['size'] < 200000;
@@ -9,19 +10,19 @@ const existingCards = (path) => (card) => {
 
 const createFolder = fs.ensureDir;
 
-const downloadCardTo = (path) => (card) => axios({
+const downloadCardTo = (target) => (card) => axios({
         url: card.imagesrc,
         method: 'GET',
         responseType: "stream" })
     .then(response =>
-        response.data.pipe(fs.createWriteStream(cardPath(card, path))))
+        response.data.pipe(fs.createWriteStream(cardPath(card, target))))
     .catch(console.error)
 
-const download = async (path, data) => {
-    return createFolder(path)
+const download = async (imgFolder, data) => {
+    return createFolder(imgFolder)
         .then(() => Promise.all(data
-            .filter(existingCards(path))
-            .map(downloadCardTo(path))
+            .filter(existingCards(imgFolder))
+            .map(downloadCardTo(imgFolder))
         ));
 }
 
