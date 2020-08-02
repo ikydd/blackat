066b2991fd937ecf513219c4211187a45da738f4
Iain Kydd <iain.kydd@gmail.com>
2020-08-02 21:30:30 +0100

    Fix #30 - order type filters

--- import/src/types/process.js
+++ import/src/types/process.js
@@ -1,9 +1,13 @@
+const typeList = ['identity', 'program', 'hardware', 'resource',
+'event', 'agenda', 'ice', 'asset', 'upgrade', 'operation'];
+
 const process = ({ data: types }) => types
     .filter(({ is_subtype }) => !is_subtype)
     .map(({ name, code, side_code }) => ({
         code,
         name,
         side: side_code
-    }));
+    }))
+    .sort((a, b) => typeList.indexOf(a.code) > typeList.indexOf(b.code) ? 1 : -1);
 
 module.exports = process
--- import/src/types/process.test.js
+++ import/src/types/process.test.js
@@ -7,22 +7,29 @@ describe('process factions', () => {
     it('outputs the correct number of types', () => {
         const output = process(mockData);
 
-        expect(output.length).toEqual(5);
+        expect(output).toHaveLength(5);
+    });
+
+    it('orders the output', () => {
+        const output = process(mockData)
+            .map(({ code }) => code);
+
+        expect(output).toEqual(['identity', 'program', 'event', 'agenda', 'ice']);
     });
 
     it('outputs the names', () => {
         const output = process(mockData);
 
-        expect(output[0].name).toEqual(mockData.data[0].name);
-        expect(output[1].name).toEqual(mockData.data[1].name);
-        expect(output[2].name).toEqual(mockData.data[2].name);
+        expect(output[0].name).toEqual("Identity");
+        expect(output[1].name).toEqual("Program");
+        expect(output[2].name).toEqual("Event");
     });
 
     it('outputs the side', () => {
         const output = process(mockData);
 
-        expect(output[2].side).toEqual(mockData.data[2].side_code);
-        expect(output[3].side).toEqual(mockData.data[3].side_code);
+        expect(output[2].side).toEqual("runner");
+        expect(output[3].side).toEqual("corp");
     });
 
     it('outputs empty side codes', () => {
@@ -35,10 +42,10 @@ describe('process factions', () => {
     it('outputs the type code', () => {
         const output = process(mockData);
 
-        expect(output[0].code).toEqual(mockData.data[0].code);
-        expect(output[1].code).toEqual(mockData.data[1].code);
-        expect(output[2].code).toEqual(mockData.data[2].code);
-        expect(output[3].code).toEqual(mockData.data[3].code);
+        expect(output[0].code).toEqual("identity");
+        expect(output[1].code).toEqual("program");
+        expect(output[2].code).toEqual("event");
+        expect(output[3].code).toEqual("agenda");
     });
 
     it('does not output subtypes', () => {
