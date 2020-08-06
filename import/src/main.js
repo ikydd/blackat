6e0864b943504ed86938b13fc203729d913bef09
Iain Kydd <iain.kydd@gmail.com>
2020-08-06 21:10:08 +0100

    #17 add empty subtypes call

--- import/src/main.js
+++ import/src/main.js
@@ -2,12 +2,14 @@ const cards = require("./cards/import");
 const factions = require("./factions/import");
 const types = require("./types/import");
 const packs = require("./packs/import");
+const subtypes = require("./subtypes/import");
 
 const run = async () => {
   const packData = await packs();
-  await cards(packData);
+  const cardData = await cards(packData);
   await factions();
   await types();
+  await subtypes(cardData);
 };
 
 module.exports = {
--- import/src/main.test.js
+++ import/src/main.test.js
@@ -2,7 +2,9 @@ const cards = require("./cards/import");
 const factions = require("./factions/import");
 const types = require("./types/import");
 const packs = require("./packs/import");
+const subtypes = require("./subtypes/import");
 const mockPackData = require("../../fixtures/api/packs");
+const mockCardData = require("../../fixtures/api/cards");
 
 const main = require("./main");
 
@@ -10,10 +12,12 @@ jest.mock("./cards/import");
 jest.mock("./factions/import");
 jest.mock("./types/import");
 jest.mock("./packs/import");
+jest.mock("./subtypes/import");
 
 describe("main", () => {
   beforeEach(() => {
     packs.mockImplementation(() => mockPackData);
+    cards.mockImplementation(() => mockCardData);
   });
 
   it("calls cards", async () => {
@@ -38,5 +42,11 @@ describe("main", () => {
     await main.run();
 
     expect(packs).toHaveBeenCalled();
+  });
+
+  it("calls subtypes", async () => {
+    await main.run();
+
+    expect(subtypes).toHaveBeenCalledWith(mockCardData);
   });
 });
