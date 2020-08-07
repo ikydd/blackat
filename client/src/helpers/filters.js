e84e41d26f3603188486ce492d19878216a8e060
Iain Kydd <iain.kydd@gmail.com>
2020-08-07 23:53:27 +0100

    Tidying up of helper files

--- client/src/helpers/filters.js
+++ client/src/helpers/filters.js
@@ -1,76 +1,76 @@
 
 
 const bySide = (side) => (card) => {
-    if (!side) {
-      return card;
-    }
-    if (card.side !== side) {
-      card.show = false;
-    }
+  if (!side) {
     return card;
   }
+  if (card.side !== side) {
+    card.show = false;
+  }
+  return card;
+}
 
 const byFactions = (factions) => (card) => {
-    if (!factions.length) {
-      return card;
-    }
-    if (!factions.includes(card.faction)) {
-      card.show = false;
-    }
+  if (!factions.length) {
     return card;
   }
+  if (!factions.includes(card.faction)) {
+    card.show = false;
+  }
+  return card;
+}
 
  const byTypes = (types) => (card) => {
-    if (!types.length) {
-      return card;
-    }
-    if (!types.includes(card.type)) {
-      card.show = false;
-    }
+  if (!types.length) {
     return card;
   }
+  if (!types.includes(card.type)) {
+    card.show = false;
+  }
+  return card;
+}
 
 const bySubtypes = (subtypes) => (card) => {
-    if (!subtypes.length) {
-      return card;
-    }
-    if (!subtypes.filter((subtype) => card.keywords && card.keywords.search(subtype) !== -1).length) {
-      card.show = false;
-    }
+  if (!subtypes.length) {
     return card;
   }
+  if (!subtypes.filter((subtype) => card.keywords && card.keywords.search(subtype) !== -1).length) {
+    card.show = false;
+  }
+  return card;
+}
 
 const byPacks = (packs) => (card) => {
-    if (!packs.length) {
-      return card;
-    }
-    if (!packs.includes(card.pack)) {
-      card.show = false;
-    }
+  if (!packs.length) {
     return card;
   }
+  if (!packs.includes(card.pack)) {
+    card.show = false;
+  }
+  return card;
+}
 
 const byTitle = (title) => (card) => {
-    if (!title) {
-      return card;
-    }
-    const regex = new RegExp(title, 'i');
-    if (card.title.search(regex) < 0) {
-      card.show = false;
-    }
+  if (!title) {
     return card;
   }
+  const regex = new RegExp(title, 'i');
+  if (card.title.search(regex) < 0) {
+    card.show = false;
+  }
+  return card;
+}
 
 const byText = (text) => (card) => {
-    if (!text) {
-      return card;
-    }
-    const regex = new RegExp(text, 'i');
-    if (card.text.search(regex) < 0) {
-      card.show = false;
-    }
+  if (!text) {
     return card;
   }
+  const regex = new RegExp(text, 'i');
+  if (card.text.search(regex) < 0) {
+    card.show = false;
+  }
+  return card;
+}
 
 export default {
     byFactions,
--- client/src/helpers/sort.js
+++ client/src/helpers/sort.js
@@ -11,30 +11,29 @@ const typeOrder = [
     'operation'
 ];
 
-const compare = (a, b) => a > b
-      ? 1
-      : (a < b
-        ? -1
-        : 0);
+const compare = (a, b) =>
+  a > b
+    ? 1
+    : (a < b
+      ? -1
+      : 0);
 
 const getFactionForSort = ({ faction }) => faction.search('neutral') !== -1 ? 'zzzzzz' : faction;
 
 const sort = (type, a, b, result = 0) => {
-    if (result !== 0) {
-      return result;
-    }
-    switch (type) {
-      case 'type':
-        return compare(typeOrder.indexOf(a.type), typeOrder.indexOf(b.type));
-      case 'faction':
-        return compare(getFactionForSort(a), getFactionForSort(b));
-      case 'name':
-      default:
-        return compare(a.title, b.title);
-    }
+  if (result !== 0) {
+    return result;
   }
-
-
+  switch (type) {
+    case 'type':
+      return compare(typeOrder.indexOf(a.type), typeOrder.indexOf(b.type));
+    case 'faction':
+      return compare(getFactionForSort(a), getFactionForSort(b));
+    case 'name':
+    default:
+      return compare(a.title, b.title);
+  }
+}
 
 export default (type) => (a, b) => {
     switch (type) {
