1be97122e0099b4c2454bd27d460c120578cf371
Iain Kydd <iain.kydd@gmail.com>
2020-08-08 18:10:25 +0100

    Remove redundant nbsp

--- client/src/components/CardSection.js
+++ client/src/components/CardSection.js
@@ -10,7 +10,6 @@ class CardSection extends Component {
             <div id={info ? info.code + '-section' : 'default-section'}>
                 {info ? (<Divider name={info.name} code={info.code}></Divider>) : ''}
                 {cards.map((card, index) => (<Card key={index} data={card} />))}
-                &nbsp;
             </div>
         );
     }
