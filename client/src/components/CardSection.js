e131a4b01a6b5f3dfb4de349d89ff54ba5f6bfba
Iain Kydd <iain.kydd@gmail.com>
2020-08-09 23:52:59 +0100

    Minor clarity tweaks in version control method

--- client/src/components/CardSection.js
+++ client/src/components/CardSection.js
@@ -4,22 +4,22 @@ import Card from './Card';
 import './CardSection.css';
 
 class CardSection extends Component {
-    versionControl (last, card) {
-        let showing = card.show !== false;
-        if (showing && last && last.title === card.title) {
+    versionControl (last, current) {
+        let showing = current.show !== false;
+        if (showing && last && last.title === current.title) {
             last.show = false;
         }
-        return showing ? card : last;
+        return showing ? current : last;
     }
 
     render() {
         const { info, cards, show } = this.props.section;
-        let lastVisible
+        let lastCard;
         return (
             <div id={info ? info.code + '-section' : 'default-section'} hidden={show === false ? 'hidden' : false} >
                 {info ? (<Divider name={info.name} code={info.code}></Divider>) : ''}
                 {cards.map((card, index) => {
-                    lastVisible = this.versionControl(lastVisible, card);
+                    lastCard = this.versionControl(lastCard, card);
                     return (<Card key={index} data={card} />);
                 } )}
             </div>
