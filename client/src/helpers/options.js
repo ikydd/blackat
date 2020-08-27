63166b37ffd2541eba7e44cab77b8b4c7240d602
Iain Kydd <iain.kydd@gmail.com>
2020-08-27 18:04:58 +0100

    Add missing options file

---
+++ client/src/helpers/options.js
@@ -0,0 +1,20 @@
+const options =  [
+    {
+        title: "Faction",
+        value: "faction"
+    },
+    {
+        title: "Type",
+        value: "type"
+    },
+    {
+        title: "Pack",
+        value: "pack"
+    },
+    {
+        title: "Title",
+        value: "title"
+    }
+]
+
+export default options;
