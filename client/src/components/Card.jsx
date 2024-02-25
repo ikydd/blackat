388b302ce0882ab4527d1c0ddb11196485e59840
Iain Kydd <iain.kydd@gmail.com>
2024-02-25 19:25:19 +0000

    Remove redundant class on Card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './Card.css';
 
-const showCard = (show) => (show === false ? '' : ' show');
-
 const hideCard = (show) => (show === false ? 'hidden' : false);
 
 const Card = ({ data: { title, imagesrc, show } }) => {
   return (
-    <div className={`card-tile${showCard(show)}`} title={title} hidden={hideCard(show)}>
+    <div className={'card-tile'} title={title} hidden={hideCard(show)}>
       <img src={imagesrc} alt={title} loading="lazy" />
     </div>
   );
