867f586704355651c2eb2ea59106e3f13325d2d4
Iain Kydd <iain.kydd@gmail.com>
2024-03-18 20:40:32 +0000

    Change filter groups into heading / buttons

--- client/src/components/FilterHeading.jsx
+++ client/src/components/FilterHeading.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import FilterNotification from './FilterNotification';
+import './FilterHeading.css';
 
 const FilterHeading = ({ title, inUse, onClick }) => (
-  <h4 className="filter-list-title" onClick={onClick}>
-    {title} {<FilterNotification on={inUse} />}
+  <h4 className="filter-list-title">
+    <button onClick={onClick}>
+      {title} {<FilterNotification on={inUse} />}
+    </button>
   </h4>
 );
 
