c9fce84190c804b2de675d32415a178da6c3972f
Iain Kydd <iain.kydd@gmail.com>
2024-03-18 22:28:26 +0000

    Change divider to h2 for page structure

--- client/src/components/Divider.jsx
+++ client/src/components/Divider.jsx
@@ -3,9 +3,9 @@ import Icon from './Icon';
 import './Divider.css';
 
 const Divider = ({ name, code }) => (
-  <h3 role="separator" className="card-divider">
+  <h2 className="card-divider">
     {name} <Icon code={code} />
-  </h3>
+  </h2>
 );
 
 export default Divider;
