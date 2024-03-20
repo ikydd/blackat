49b458c9201d9b4e77c56877b28faac9af768926
Iain Kydd <iain.kydd@gmail.com>
2024-03-20 13:10:52 +0000

    Rough implementation of show hide controls

--- client/src/components/ControlPanel.jsx
+++ client/src/components/ControlPanel.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Icon from './Icon';
 import meta from '../../../package.json';
 import './ControlPanel.css';
 
-const ControlPanel = ({ children }) => (
-  <div id="control-panel">
-    <h2 className="sr-only">Card Filter Controls</h2>
-    <div id="filters">{children}</div>
-    <div id="addendum">
-      <p id="version">Version {meta.version}</p>
-      <p>
-        <a href="#small-print">Copyright notice</a>
-      </p>
+const ControlPanel = ({ children }) => {
+  const [hidden, setHidden] = useState(false);
+  return (
+    <div id="control-panel">
+      <h2 className="sr-only">Card Filter Controls</h2>
+      <div id="filters" hidden={hidden}>
+        {children}
+      </div>
+      <div id="addendum" hidden={hidden}>
+        <p id="version">Version {meta.version}</p>
+        <p>
+          <a href="#small-print">Copyright notice</a>
+        </p>
+      </div>
+      <div className="mobile-only">
+        <button
+          id="control-display-toggle"
+          aria-expanded={!hidden}
+          aria-controls="filters"
+          className={hidden ? 'closed' : 'open'}
+          onClick={() => setHidden(!hidden)}
+        >
+          <Icon code="click" /> {hidden ? 'Controls' : 'Hide'}
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ControlPanel;
