22585ea0d691f0d9abf88cf494c325494cd7f854
Iain Kydd <iain.kydd@gmail.com>
2024-03-20 17:32:28 +0000

    Make touch focus persistent

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
 const hideCard = (show) => (show === false ? 'hidden' : false);
 
 const Card = ({ data: { title, imagesrc, show } }) => {
+  const [focus, setFocus] = useState(false);
   return (
     <div
-      className={`card-tile${show ? ' visible-card' : ''}`}
+      className={`card-tile${show ? ' visible-card' : ''}${focus ? ' focused-card' : ''}`}
       title={title}
       hidden={hideCard(show)}
     >
-      <img src={imagesrc} alt={title} loading="lazy" />
+      <img
+        src={imagesrc}
+        alt={title}
+        loading="lazy"
+        onTouchStart={() => {
+          setFocus(!focus);
+        }}
+      />
     </div>
   );
 };
