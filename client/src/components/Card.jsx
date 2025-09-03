08c90227fffb8e1c98ad183d0e3570049287e14d
ikydd <iain.kydd@gmail.com>
2025-09-03 21:36:16 +0100

    Make image blur in mouse mode

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 import './Card.css';
 
 const Card = ({ data: { title, imagesrc } }) => {
+  let aboutToFocus;
   return (
     <div className="card-tile" tabIndex="0" title={title}>
-      <img loading="lazy" src={imagesrc} alt={title} />
+      <img
+        loading="lazy"
+        src={imagesrc}
+        alt={title}
+        onTouchStart={() => {
+          aboutToFocus = true;
+        }}
+        onTouchMove={() => {
+          aboutToFocus = false;
+        }}
+        onTouchEnd={(e) => {
+          if (aboutToFocus && e.target === document.activeElement.getElementsByTagName('img')[0]) {
+            document.activeElement.blur();
+            e.preventDefault();
+          }
+        }}
+      />
     </div>
   );
 };
