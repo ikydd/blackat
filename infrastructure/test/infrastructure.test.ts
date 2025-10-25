def2f10c7be6c3a666e7bc9464db6c328dd65154
ikydd <iain.kydd@gmail.com>
2025-10-26 00:14:49 +0100

    Break up infra tests

--- infrastructure/test/infrastructure.test.ts
+++ infrastructure/test/infrastructure.test.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Template, Match } from 'aws-cdk-lib/assertions';
 import { StaticAssetsStack } from '../lib/static-assets';
 
-test('Static assets Bucket created', () => {
+test('Static assets bucket created', () => {
   const app = new cdk.App();
   const stack = new StaticAssetsStack(app, 'BlackatStaticAssets');
   const template = Template.fromStack(stack);
@@ -10,6 +10,12 @@ test('Static assets Bucket created', () => {
   template.hasResourceProperties('AWS::S3::Bucket', {
     BucketName: 'static.blackat.co.uk'
   });
+});
+
+test('CDN is fronting the bucket created', () => {
+  const app = new cdk.App();
+  const stack = new StaticAssetsStack(app, 'BlackatStaticAssets');
+  const template = Template.fromStack(stack);
 
   template.hasResourceProperties('AWS::CloudFront::Distribution', {
     DistributionConfig: {
